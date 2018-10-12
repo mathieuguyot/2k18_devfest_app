fb4801314009fc1ff7806133abf2c6f47f328c0d
mathieu guyot <mathieuguyot40@yahoo.fr>
2018-10-12 15:45:17 +0200

    Init redux/redux-saga archi

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import HomeComp from './components/HomeComp';
+import HomeComp from './src/components/HomeComp';
 import { Header } from 'react-native-elements';
+import createStore from './src/redux';
+import { Provider } from 'react-redux';
+
+const store = createStore();
 
 export default class App extends React.Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <Header
-          backgroundColor= '#bf5022'
-          leftComponent={{ text: 'DEVFEST', style: { color: '#fff' } }}
-          rightComponent={{ icon: 'menu', color: '#fff' }}
-        />
-        <HomeComp />
-      </View>
+      <Provider store={store}>
+        <View style={styles.container}>
+          <Header
+            backgroundColor= '#bf5022'
+            leftComponent={{ text: 'DEVFEST', style: { color: '#fff' } }}
+            rightComponent={{ icon: 'menu', color: '#fff' }}
+          />
+          <HomeComp />
+        </View>
+      </Provider>
     );
   }
 
