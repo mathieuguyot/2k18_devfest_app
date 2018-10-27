07c22ae06c224a209265163ab8935a4f954657a4
NicolasGn <nicolas.c.guegan@gmail.com>
2018-10-27 14:08:07 +0200

    Cache is not properly working (disabled)

--- src/components/HomeComp.js
+++ src/components/HomeComp.js
@@ -2,14 +2,22 @@ import React, { Component } from 'react';
 import { StyleSheet, View, Image, Dimensions } from 'react-native';
 import { Button, Text, Icon } from 'react-native-elements';
 import { connect } from 'react-redux';
-import { fetchSpeakers, rehydrateSpeakers } from '../redux/state/speakers/index';
-import { fetchSessions, rehydrateSessions } from '../redux/state/sessions/index';
-import { fetchSchedule, rehydrateSchedule } from '../redux/state/schedule/index';
+import {
+  fetchSpeakers,
+  rehydrateSpeakers
+} from '../redux/state/speakers/index';
+import {
+  fetchSessions,
+  rehydrateSessions
+} from '../redux/state/sessions/index';
+import {
+  fetchSchedule,
+  rehydrateSchedule
+} from '../redux/state/schedule/index';
 
 class HomeComp extends Component {
-  
   static navigationOptions = {
-    title: 'DevFest Nantes 2018',
+    title: 'DevFest Nantes 2018'
   };
 
   onSessionButtonClicked() {
@@ -23,101 +31,100 @@ class HomeComp extends Component {
   onPhoneInfoButtonClicked() {
     this.props.navigation.navigate('DeviceInfos');
   }
-  
 
   componentDidMount() {
-    if(!this.props.speakersLoaded) {
+    if (!this.props.speakersLoaded) {
       this.props.dispatch(fetchSpeakers());
     } else {
-      console.log("speakers loaded from phone memory");
+      console.log('speakers loaded from phone memory');
       this.props.dispatch(rehydrateSpeakers(this.props.speakersJsonStr));
     }
 
-    if(!this.props.sessionsLoaded) {
+    if (!this.props.sessionsLoaded) {
       this.props.dispatch(fetchSessions());
     } else {
-      console.log("sessions loaded from phone memory");
+      console.log('sessions loaded from phone memory');
       this.props.dispatch(rehydrateSessions(this.props.sessionJsonStr));
     }
-    
-    if(!this.props.timeSlotsLoaded) {
+
+    if (true /* Cache is not working properly !this.props.timeSlotsLoaded*/) {
       this.props.dispatch(fetchSchedule());
     } else {
-      console.log("schedules loaded from phone memory");
-      this.props.dispatch(rehydrateSchedule(this.props.timeSlotJsonStr));
+      console.log('schedules loaded from phone memory');
+      console.log(this.props.scheduleJsonStr);
+      this.props.dispatch(rehydrateSchedule(this.props.scheduleJsonStr));
     }
   }
 
   render() {
-      return (
-        <View style={styles.container}>
-          <Image
-            source={require('../../assets/devfest_logo.png')}
-            style={styles.logo}
-          />
-          <Button 
-            title='Voir les sessions'
-            icon={
-              <Image
-                source={require('../../assets/planet1_logo.png')}
-                style={styles.planet1_logo}
-              />
-            }
-            buttonStyle={{
-              backgroundColor: '#bf5022',
-              width: 300,
-              height: 45,
-              borderColor: "#ffffff",
-              borderRadius: 100,
-              borderWidth: 0,
-              elevation: 0,
-            }}
-            onPress={this.onSessionButtonClicked.bind(this)}
-          />
-          <Button
-            title='Voir les présentateurs'
-            icon={
-              <Image
-                source={require('../../assets/planet2_logo.png')}
-                style={styles.planet1_logo}
-              />
-            }
-            buttonStyle={{
-              marginTop: 25,
-              backgroundColor: '#bf5022',
-              width: 300,
-              height: 45,
-              borderColor: "#ffffff",
-              borderRadius: 100,
-              borderWidth: 0,
-              elevation: 0,
-            }}
-            onPress={this.onSpeakerButtonClicked.bind(this)}
-          />
-          <Button 
-            title='Informations du téléphone'
-            icon={
-              <Image
-                source={require('../../assets/planet2_logo.png')}
-                style={styles.planet1_logo}
-              />
-            }
-            buttonStyle={{
-              marginTop: 25,
-              backgroundColor: '#bf5022',
-              width: 300,
-              height: 45,
-              borderColor: "#ffffff",
-              borderRadius: 100,
-              borderWidth: 0,
-              elevation: 0,
-            }}
-            onPress={this.onPhoneInfoButtonClicked.bind(this)}
-          />
-        </View>
+    return (
+      <View style={styles.container}>
+        <Image
+          source={require('../../assets/devfest_logo.png')}
+          style={styles.logo}
+        />
+        <Button
+          title="Voir les sessions"
+          icon={
+            <Image
+              source={require('../../assets/planet1_logo.png')}
+              style={styles.planet1_logo}
+            />
+          }
+          buttonStyle={{
+            backgroundColor: '#bf5022',
+            width: 300,
+            height: 45,
+            borderColor: '#ffffff',
+            borderRadius: 100,
+            borderWidth: 0,
+            elevation: 0
+          }}
+          onPress={this.onSessionButtonClicked.bind(this)}
+        />
+        <Button
+          title="Voir les présentateurs"
+          icon={
+            <Image
+              source={require('../../assets/planet2_logo.png')}
+              style={styles.planet1_logo}
+            />
+          }
+          buttonStyle={{
+            marginTop: 25,
+            backgroundColor: '#bf5022',
+            width: 300,
+            height: 45,
+            borderColor: '#ffffff',
+            borderRadius: 100,
+            borderWidth: 0,
+            elevation: 0
+          }}
+          onPress={this.onSpeakerButtonClicked.bind(this)}
+        />
+        <Button
+          title="Informations du téléphone"
+          icon={
+            <Image
+              source={require('../../assets/planet2_logo.png')}
+              style={styles.planet1_logo}
+            />
+          }
+          buttonStyle={{
+            marginTop: 25,
+            backgroundColor: '#bf5022',
+            width: 300,
+            height: 45,
+            borderColor: '#ffffff',
+            borderRadius: 100,
+            borderWidth: 0,
+            elevation: 0
+          }}
+          onPress={this.onPhoneInfoButtonClicked.bind(this)}
+        />
+      </View>
     );
   }
-
 }
 
 const windowWidth = Dimensions.get('window').width;
@@ -127,15 +134,15 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#3d4746',
+    backgroundColor: '#3d4746'
   },
   logo: {
     width: windowWidth * 0.75,
-    resizeMode: "contain",
+    resizeMode: 'contain'
   },
   planet1_logo: {
     width: 35,
-    resizeMode: 'contain',
+    resizeMode: 'contain'
   }
 });
 
@@ -146,8 +153,11 @@ const mapStateToProps = state => {
     sessionsLoaded: state.sessions.sessionsLoaded,
     speakersJsonStr: state.speakers.speakersJsonStr,
     sessionJsonStr: state.sessions.sessionJsonStr,
-    timeSlotJsonStr: state.schedule.timeSlotJsonStr
-  }
-}
+    scheduleJsonStr: state.schedule.scheduleJsonStr
+  };
+};
 
-export default connect(mapStateToProps, null)(HomeComp);
+export default connect(
+  mapStateToProps,
+  null
+)(HomeComp);
--- src/models/Schedule.js
+++ src/models/Schedule.js
@@ -3,11 +3,10 @@ import ScheduleDay from './ScheduleDay';
 
 // Immutable container of timeSlots
 export default class Schedule {
-  
   // This constructor may fail regarding of the json data provided
   // Catch any construction of a new time slot container
-  constructor(timeSlotsJsonData) {
-    this.days = timeSlotsJsonData.map(
+  constructor(scheduleJsonData) {
+    this.days = scheduleJsonData.map(
       d => new ScheduleDay(d.date, d.dateReadable, d.tracks, d.timeslots)
     );
   }
