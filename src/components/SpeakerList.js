ecc3ba1ab76d8ac95be39a5340a3fc7c47b2a8e5
NicolasGn <nicolas.c.guegan@gmail.com>
2018-10-27 17:53:37 +0200

    Speaker list

--- src/components/SpeakerList.js
+++ src/components/SpeakerList.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, ScrollView, TouchableOpacity } from 'react-native';
-import { Text } from 'react-native-elements';
+import { Text, Avatar } from 'react-native-elements';
 import { connect } from 'react-redux';
+
+import { imageServerUrl } from '../api';
+
+const AVATAR_SIZE = 60;
 
 class SpeakerList extends Component {
   constructor(props) {
     super(props);
+    this.navigation = props.navigation;
   }
 
   static navigationOptions = {
@@ -13,43 +18,44 @@ class SpeakerList extends Component {
   };
 
   render() {
-    let speakersRows = [];
-    for (let speakerKey of this.props.speakerContainer.getSpeakersKeys()) {
-      let item = this.props.speakerContainer.getSpeaker(speakerKey);
-      speakersRows.push(
-        <SpeakerListItem
-          props={this.props}
-          speakerKey={speakerKey}
-          key={speakerKey}
-          speakerName={item.name}
-        />
-      );
-    }
+    const { speakerContainer } = this.props;
+    const speakers = speakerContainer
+      .getSpeakersKeys()
+      .map(id => speakerContainer.getSpeaker(id))
+      .sort((a, b) => (a < b ? -1 : 1));
     return (
-      <View style={{ flex: 1 }}>
+      <View style={{ flex: 1, backgroundColor: '#3d4746' }}>
         <ScrollView>
           <View style={styles.container}>
-            <View>{speakersRows}</View>
+            {speakers.map(speaker => (
+              <View key={speaker.speakerId} style={styles.speakerItem}>
+                <TouchableOpacity
+                  onPress={() =>
+                    this.navigation.navigate('Speaker', {
+                      speakerKey: speaker.speakerId
+                    })
+                  }
+                  style={styles.touchableItem}
+                >
+                  <View style={styles.speakerAvatar}>
+                    <Avatar
+                      width={AVATAR_SIZE}
+                      height={AVATAR_SIZE}
+                      source={{
+                        uri: `${imageServerUrl}${speaker.speakerPhotoPath}`
+                      }}
+                    />
+                  </View>
+                  <Text style={styles.speakerName}>{speaker.name}</Text>
+                </TouchableOpacity>
+              </View>
+            ))}
           </View>
         </ScrollView>
       </View>
     );
   }
 }
-
-const SpeakerListItem = ({ props, speakerName, speakerKey }) => (
-  <TouchableOpacity
-    style={styles.sessionListItem}
-    onPress={() => {
-      props.navigation.navigate('Speaker', { speakerKey: speakerKey });
-    }}
-  >
-    <View style={styles.sessionItemBadge} />
-    <View styles={styles.sessionItemTextbox}>
-      <Text style={styles.sessionItemTitle}>{speakerName}</Text>
-    </View>
-  </TouchableOpacity>
-);
 
 const styles = StyleSheet.create({
   container: {
@@ -59,21 +65,31 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
     alignItems: 'flex-start'
   },
-  sessionListItem: {
-    height: 50,
+  speakerItem: {
+    height: AVATAR_SIZE,
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'flex-start',
     alignItems: 'center',
     marginBottom: 20
   },
-  sessionItemBadge: {
+  touchableItem: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center'
+  },
+  speakerName: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    color: 'white'
+  },
+  speakerAvatar: {
     borderRadius: 9999,
-    width: 50,
-    height: 50,
-    backgroundColor: 'white',
-    marginRight: 10,
-    marginLeft: 3
+    width: AVATAR_SIZE,
+    height: AVATAR_SIZE,
+    overflow: 'hidden',
+    marginRight: 15
   }
 });
 
